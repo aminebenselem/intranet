8a945acd318c3789ba5261d17a666487eb4b527c
Obito511 <abensaid511@gmail.com>
2023-02-16 18:22:41 +0100

    new

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./header.component.css']
   })
   export class HeaderComponent implements OnInit {
-  
-     ngOnInit(): void {
-       
-     }
-   
+    constructor() { }
+    menu_icon_variable: boolean = false;
+    menuVariable: boolean = false;
+    ngOnInit(): void {
+    }
+    openMenu() {
+      this.menuVariable =! this.menuVariable;
+      this.menu_icon_variable =! this.menu_icon_variable;
+    }
+     
   
 
 }
