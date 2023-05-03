8682e485f33ad30c35420adbf9f80fe23026c313
Obito511 <abensaid511@gmail.com>
2023-05-03 19:27:03 +0100

    akali

--- src/app/demandes/demandes.component.ts
+++ src/app/demandes/demandes.component.ts
@@ -7,6 +7,8 @@ import { DownloadService } from '../services/downloadservice.service';
 import { error } from 'console';
 import { map } from 'rxjs';
 import { fichedepaie } from './fichedepaie';
+import { demande } from './demande';
+import { FileDetails } from '../FileDetails';
 
 @Component({
   selector: 'app-demandes',
@@ -24,16 +26,26 @@ demandes:any=[
   'Demande sociale',
   'Demande de congé'
 ];
+filename:string=""
+file!: File;
+fileDetails!: FileDetails;
+fileUris: Array<string> = [];
+filenameb:string=""
+fileb!: File;
+fileDetailsb!: FileDetails;
+fileUrisb: Array<string> = [];
 socials:any=[
   'demande avance sur salaire',
   'demande prime de déces',
   'demande prime de mariage',
  'demande convention Ooredoo',
 ];
-social1:String='';
+social1:string='';
 fichedepaie:fichedepaie=new fichedepaie()
+demande:demande=new demande()
 token=this.storage.getToken();
 baseURL:String="http://localhost:9090/fichedepaie"
+baseurl:String="http://localhost:9090"
 header=new HttpHeaders()
    .set("authorization","Bearer "+this.token);
   constructor(private ss:DownloadService,private http:HttpClient,private storage:StorageService,private route:Router,private fileUploadService: FileUploadService) { }
@@ -73,7 +85,7 @@ changeHandler2(event:any){
 }
 
 getAttestation(){
-  this.download(this.baseURL+"/attestation","attestation.pdf")
+  this.download(this.baseurl+"/attestation","attestation.pdf")
 
 }
 
@@ -113,4 +125,112 @@ complete: () => console.log("")
 
 });
 }
+postDemande(){
+  this.demande.user.mat_Pers=this.storage.getUser();
+  this.demande.type=this.social1
+  this.demande.proved="en cours"
+  this.filename=this.getFilename(this.file)
+  this.uploadFile()
+  this.demande.uri=this.getFileUri()
+
+let data=JSON.parse( JSON.stringify(this.demande));
+
+console.log(data)
+return this.http.post(this.baseurl+"/newdemande",data,{headers:this.header})
+.subscribe({
+  next: (res) => {this.response=res,console.log(res)},
+error: (err) => {console.log(err)},
+complete: () => console.log("")
+
+});
+}
+
+
+reloadPage() {
+  window.location.reload();
+}
+selectFile(event: any) {
+  this.file = event.target.files.item(0);
+  console.log(this.file.name)
+  
+}
+ getFileUri(){
+ 
+  return this.demande.uri="http://localhost:9090/image/"+this.filename;
+}
+uploadFile() {
+  this.fileUploadService.upload(this.file,this.filename,"/upload").subscribe({
+    next: (data) => {
+      {this.fileDetails = data,console.log(data)};
+      this.fileUris.push(this.fileDetails.fileUri);
+      alert("File Uploaded Successfully")
+    },
+    error: (e) => {
+      console.log(e);
+    }
+  });
+}
+
+  
+
+  getWord(minLength: number = 4, maxLength: number = 10):any {
+    return Math.trunc(Math.random() * (100000));
+  
+  }
+  getFilename(file: File){
+    let filename=file.name
+    let extension =filename.substring(filename.lastIndexOf('.')+1, filename.length) || filename;
+   return filename.substring(0,filename.lastIndexOf('.')-1)+this.getWord()+'.'+extension;
+  
+  }
+  getFileUrib(file:File){
+ 
+    return this.demande.uri="http://localhost:9090/image/"+this.getFilename(file);
+    
+    }
+  postDemandeb(file1:File,file2:File){
+
+
+    this.demande.user.mat_Pers=this.storage.getUser();
+    this.demande.type=this.social1
+    this.demande.proved="en cours"
+    let filename1=this.getFilename(file1)
+    let filename2=this.getFilename(file2)
+    this.demande.uri=this.getFileUrib(file1)
+     this.demande.uri1=this.getFileUrib(file2)
+
+  let data=JSON.parse( JSON.stringify(this.demande));
+  this.uploadFileb(file1,filename1)
+  this.uploadFileb(file2,filename2)
+
+  console.log(data)
+  return this.http.post(this.baseurl+"/newdemande",data,{headers:this.header})
+  .subscribe({
+    next: (res) => {this.response=res,console.log(res)},
+  error: (err) => {console.log(err)},
+  complete: () => console.log("")
+
+  });
+
+}
+
+uploadFileb(file:File,filename:any) {
+this.fileUploadService.upload(file,filename,"/upload").subscribe({
+  next: (data) => {
+    {this.fileDetails = data,console.log(data)};
+    this.fileUris.push(this.fileDetails.fileUri);
+    alert("File Uploaded Successfully")
+  },
+  error: (e) => {
+    console.log(e);
+  }
+});
+}
+ 
+  selectFileb(event1: any) {
+    this.fileb = event1.target.files.item(0);
+
+    console.log(this.fileb.name)
+  }
+  
 }
--- src/app/temps-de-travail/tableaudeservice/tableaudeservice.component.ts
+++ src/app/temps-de-travail/tableaudeservice/tableaudeservice.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { equal } from 'assert';
+import { DownloadService } from 'src/app/services/downloadservice.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-tableaudeservice',
@@ -6,10 +10,386 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tableaudeservice.component.css']
 })
 export class TableaudeserviceComponent implements OnInit {
-
-  constructor() { }
+  response?:any;
+  fevrier?:any;
+  Mars?:any;
+  Avril?:any;
+  Mai?:any;
+  Juin?:any;
+  Juillet?:any;
+  Aout?:any;
+  Septembre?:any;
+  Octobre?:any;
+  Novembre?:any;
+  Decembre?:any;
+  daysimpl:any=[];
+    a:any;
+    janvierDays :any= [];
+    token=this.storage.getToken();
+    baseURL:String="http://localhost:9090"
+    backup:any=[]
+    janvier:any=[]
+    header=new HttpHeaders()
+       .set("authorization","Bearer "+this.token)
+       header1=new HttpHeaders()
+       .set("authorization","Bearer "+this.token)
+       .set("Content-Type","image/jpg");
+  
+    constructor(private http:HttpClient,private storage:StorageService,private ss:DownloadService) { }
 
   ngOnInit(): void {
+    this.getTableau()
+
+  }
+  getTableau(){
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.response=res;
+       this.backup=this.response
+     this.getJanvier();
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+  getJanvier() {
+    console.log(this.backup)
+    this.janvierDays.length = 0;
+    for (let i=0;i<this.backup.length;i++){
+      if (this.backup[i].months==1){
+  
+
+  this.janvierDays.push(this.backup[i])
+
+}
+}
+
+      
+console.log(this.janvierDays)
+  
+
   }
 
+
+  getTableauFevrier(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.fevrier=res;
+      
+       this.backup=this.fevrier
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==2){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+  getTableauMars(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Mars=res;
+      
+       this.backup=this.Mars
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==3){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+  getTableauAvril(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Avril=res;
+      
+       this.backup=this.Avril
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==4){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauMai(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Mai=res;
+      
+       this.backup=this.Mai
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==5){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+  getTableauJuin(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Juin=res;
+      
+       this.backup=this.Juin
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==6){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauJuillet(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Juillet=res;
+      
+       this.backup=this.Juillet
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==7){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauAout(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Aout=res;
+      
+       this.backup=this.Aout
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==8){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauSeptembre(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Septembre=res;
+      
+       this.backup=this.Septembre
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==9){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauOctobre(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Octobre=res;
+      
+       this.backup=this.Octobre
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==10){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+
+  getTableauNovembre(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Novembre=res;
+      
+       this.backup=this.Novembre
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==11){
+    
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+        
+        
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
+  getTableauDecembre(){
+    this.janvierDays.length = 0;
+    this.backup.length = 0;
+
+    return this.http.get(this.baseURL+"/tableau/service",{headers:this.header})
+    .subscribe({
+      next: (res) => {
+       this.Decembre=res;
+       
+    
+       this.backup=this.Decembre
+       console.log(this.backup)
+       for (let i=0;i<this.backup.length;i++){
+        if (this.backup[i].months==12){
+        
+  
+    this.janvierDays.push(this.backup[i])
+  
+  }
+  }
+
+  
+
+ 
+
+  console.log(this.janvierDays);
+  
+ 
+ 
+       
+  
+  
+
+  
+
+      },
+  error: (err) => console.log(err),
+  complete: () => console.log("")
+  
+  });
+  }
 }
